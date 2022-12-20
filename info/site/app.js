ec0c7c34d5c4965919d16e700c7377046c80ffa9
Timnini <96081549+Timnini@users.noreply.github.com>
2022-12-20 15:51:10 +0100

    Delete info directory

--- info/site/app.js
+++
@@ -1,9 +0,0 @@
-function maFonction(){
-   saisie =  document.querySelector("#zonedeSaisie").value;
-   console.log(saisie);
-}
-
-
-
-
-document.querySelector("#buton").addEventListener("click",maFonction);
