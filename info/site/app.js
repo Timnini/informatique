7ab5278aff41caa7edb4119efab531218d57c912
Timnini <96081549+Timnini@users.noreply.github.com>
2022-12-20 15:40:34 +0100

    Add files via upload

---
+++ info/site/app.js
@@ -0,0 +1,9 @@
+function maFonction(){
+   saisie =  document.querySelector("#zonedeSaisie").value;
+   console.log(saisie);
+}
+
+
+
+
+document.querySelector("#buton").addEventListener("click",maFonction);
